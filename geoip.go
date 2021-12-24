4ea157d0dd8c58fb9c574e2f4c510a8d7b949efa
Axel Vanzaghi <avanzaghi@groupeot.com>
2021-12-24 10:11:49 +0100

    geoip request

--- geoip.go
+++ geoip.go
@@ -3,69 +3,57 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 type GeoIP struct {
-	status      string  `json:"status"`
-	country     string  `json:"country"`
-	countryCode string  `json:"countryCode"`
-	region      string  `json:"region"`
-	regionName  string  `json:"regionName"`
-	city        string  `json:"city"`
-	zip         string  `json:"zip"`
-	lat         float64 `json:"lat"`
-	lon         float64 `json:"lon"`
-	timezone    string  `json:"timezone"`
-	isp         string  `json:"isp"`
-	org         string  `json:"org"`
-	as          string  `json:"as"`
-	query       string  `json:"query"`
+	Status      string  `json:"status"`
+	Country     string  `json:"country"`
+	CountryCode string  `json:"countryCode"`
+	Region      string  `json:"region"`
+	RegionName  string  `json:"regionName"`
+	City        string  `json:"city"`
+	Zip         string  `json:"zip"`
+	Lat         float64 `json:"lat"`
+	Lon         float64 `json:"lon"`
+	Timezone    string  `json:"timezone"`
+	Isp         string  `json:"isp"`
+	Org         string  `json:"org"`
+	As          string  `json:"as"`
+	Query       string  `json:"query"`
+	Message     string  `json:"message"`
 }
 
-func Request(ip string) (GeoIP, error) {
+func Request(ip string) (*GeoIP, error) {
 	var geoip GeoIP
 	req, err := http.Get("http://ip-api.com/json/" + ip)
 	// Check general error
 	if err != nil {
-		return geoip, err
+		return nil, err
 	}
 	defer req.Body.Close()
 	// Check request status
 	if req.StatusCode != 200 || (!(strings.Contains(req.Header.Get("Content-Type"), "application/json"))) {
-		return geoip, errors.New("Bad response from API")
+		return nil, errors.New("Bad response from API : " + strconv.Itoa(req.StatusCode) + "\nContent-Type : " + req.Header.Get("Content-Type"))
 	}
 	// Get body into byte array
 	body, err := ioutil.ReadAll(req.Body)
-	fmt.Println(string(body[:]))
 	if err != nil {
-		return geoip, err
+		return nil, err
 	}
 	// Unmarshal JSON
 	if !json.Valid(body) {
-		return geoip, errors.New("Unvalid JSON")
+		return nil, errors.New("Unvalid JSON")
 	}
 	err = json.Unmarshal(body, &geoip)
 	if err != nil {
-		return geoip, err
+		return nil, err
 	}
-	fmt.Println(geoip)
-	return geoip, nil
-}
-
-func test() {
-	geoip, err := Request("4.4.4.4")
-	if err != nil {
-		fmt.Println(err)
+	if geoip.Status != "success" {
+		return nil, errors.New("Request failed : " + geoip.Message)
 	}
-	fmt.Println(geoip.status)
-	fmt.Println(geoip.country)
-	fmt.Println(geoip.query)
-}
-
-func main() {
-	test()
+	return &geoip, nil
 }
